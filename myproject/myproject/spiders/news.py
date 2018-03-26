69e88c87aad80a8553911ead368924da188f08dc
tryuichi <ryuichi.takahashi@gmail.com>
2018-03-26 23:08:12 +0900

    wired.jpをクロールする部分まで

--- myproject/myproject/spiders/news.py
+++ myproject/myproject/spiders/news.py
@@ -1,4 +1,5 @@
 import scrapy
+from myproject.items import Headline
 
 class NewsSpider(scrapy.Spider):
     name = 'news'   # 実行時するSpiderの名前。実行場所はscrapy.cfgのいるところで、scrapy crawl news
@@ -15,4 +16,11 @@ class NewsSpider(scrapy.Spider):
             yield scrapy.Request(response.urljoin(url), self.parse_topics)
         
     def parse_topics(self, response):
-        pass
+        """
+        トピックスのページからタイトルと本文を抜き出す。
+        """
+        item = Headline()   # Headlineオブジェクトの作成。
+        item['title'] = response.css('.newsTitle ::text').extract_first() # タイトル
+        item['body'] = response.css('.hbody').xpath('string()').extract_first() # 本文
+        #item['body'] = ''.join(response.css('.hbody ::text').extract()) # 本文 CSSセレクターのみを使う場合
+        yield item # Itemをyieldして、データを抽出する。
