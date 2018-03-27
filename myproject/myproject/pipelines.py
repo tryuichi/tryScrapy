5186c72a43894835d09237d13869a888e9ebfc36
tryuichi <ryuichi.takahashi@gmail.com>
2018-03-27 11:45:04 +0900

    gitignoreの設定

--- myproject/myproject/pipelines.py
+++ myproject/myproject/pipelines.py
@@ -1,11 +1,19 @@
-# -*- coding: utf-8 -*-
 
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exceptions import DropItem
 
-class MyprojectPipeline(object):
+class ValidationPipeline(object):
+    """
+    Itemを検証するPipeline
+    """
     def process_item(self, item, spider):
-        return item
+        if not item['title']:
+            # titleフィールドが取得できていない場合は破棄する。
+            # DropItem()の引数は破棄する理由を表すメッセージ
+            raise DropItem('Missing title')
+        
+        return item 
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -64,9 +64,9 @@ DOWNLOAD_DELAY = 1
 
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'myproject.pipelines.MyprojectPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'myproject.pipelines.ValidationPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/autothrottle.html
